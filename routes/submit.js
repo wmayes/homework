012901336e52c69d8ec8a4e27b19dcf097e78888
Alex Mayes <AlexM@cmslaser.com>
2016-09-02 09:35:05 -0400

    adding entire project

---
+++ routes/submit.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('submit', { title: 'Homework 2' });
+});
+
+//router.post('/log', function(req, res) {
+//	console.log(req.body);
+//});
+
+module.exports = router;
